bb1d83c770e0eb656798f8a9440f2af8272d03ae
faaaaaa6 <fahanasmuhammad634@gmail.com>
2026-06-16 10:27:08 +0530

    added a custom user to the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)#for registring customuser in admin panel 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username' ,'email' , 'phone_number' , 'is_online' , 'last_seen')
+    fieldset = UserAdmin.fieldsets + (
+        
+        ('Extra_Info',{'fields':('phone_number','profile_picture','bio','is_online','last_seen')}),
+    )
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):#django automatically build username,emails,password,first name and last name.
+    phone_number = models.CharField(max_length=15, unique = True, null = True, blank = True)
+    profile_picture = models.ImageField(upload_to ='profile_pics/', null = True , blank = True)
+    bio = models.TextField(null=True, blank =True)
+    is_online = models.BooleanField(default = False)
+    last_seen  = models.DateTimeField(null = True ,blank = True)
+
+    def __str__(self):
+        return self.username
+
+#str is used to show the username instead of user id as output
